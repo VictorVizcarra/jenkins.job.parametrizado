a2290359f0903042900e7b0f2e28b7eae6e09dda
Victor Vizcarra <54164831+VictorVizcarra@users.noreply.github.com>
2022-04-06 17:54:43 -0700

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,7 +1,7 @@
 job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      		git('https://github.com/VictorVizcarra/jenkins.job.parametrizado.git', 'main') { node ->
         		node / gitConfigName('VictorVizcarra')
         		node / gitConfigEmail('victormjvizcarra@gmail.com')
       		}
